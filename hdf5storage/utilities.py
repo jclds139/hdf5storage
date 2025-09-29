e2debbcd2c594db8d5046fcc2c23378b92788d87
Randall Pittman <pittmara@oregonstate.edu>
2025-09-29 09:51:37 -0700

    Removed unused ignore

--- hdf5storage/utilities.py
+++ hdf5storage/utilities.py
@@ -172,7 +172,10 @@ class LowLevelFile:
         if not isinstance(f, h5py.File):
             msg = "f must be a h5py.File."
             raise TypeError(msg)
-        if options.__class__.__module__ != "hdf5storage" or options.__class__.__name__ != "Options":
+        if (
+            options.__class__.__module__ != "hdf5storage"
+            or options.__class__.__name__ != "Options"
+        ):
             msg = "options must be a hdf5storage.Options."
             raise TypeError(msg)
         self._f: h5py.File = f
@@ -449,7 +452,8 @@ class LowLevelFile:
                     or np.any(
                         self._canonical_empty[...] != np.array([0, 0], dtype=np.uint64),
                     )
-                    or convert_attribute_to_string(ce_attrs.get("MATLAB_class")) != "canonical empty"
+                    or convert_attribute_to_string(ce_attrs.get("MATLAB_class"))
+                    != "canonical empty"
                     or ce_attrs.get("MATLAB_empty") != 1
                 ):
                     del self._refs_group["a"]
@@ -607,7 +611,8 @@ class LowLevelFile:
                     or np.any(
                         self._canonical_empty[...] != np.array([0, 0], dtype=np.uint64),
                     )
-                    or convert_attribute_to_string(ce_attrs.get("MATLAB_class")) != "canonical empty"
+                    or convert_attribute_to_string(ce_attrs.get("MATLAB_class"))
+                    != "canonical empty"
                     or ce_attrs.get("MATLAB_empty") != 1
                 ):
                     del self._refs_group["a"]
@@ -925,7 +930,11 @@ def convert_to_numpy_str(  # noqa: C901, PLR0911, PLR0912
         if ndarray_has_type(data, np.bytes_):
             # Just decode the bytes as UTF-8
             return np.char.decode(data, "UTF-8")
-        if ndarray_has_type(data, np.uint8) or ndarray_has_type(data, np.uint16) or ndarray_has_type(data, np.uint32):
+        if (
+            ndarray_has_type(data, np.uint8)
+            or ndarray_has_type(data, np.uint16)
+            or ndarray_has_type(data, np.uint32)
+        ):
             # It is an ndarray of some uint type. How it is converted
             # depends on its shape. If its shape is just (), then it is
             # just a scalar wrapped in an array, which can be converted
@@ -971,7 +980,9 @@ def convert_to_numpy_str(  # noqa: C901, PLR0911, PLR0912
                 else:
                     encoding = "UTF-32BE"
                     dt = "S" + str(4 * length_to_use)
-                swapbytes = data.dtype.byteorder == "<" or (sys.byteorder == "little" and data.dtype.byteorder == "=")
+                swapbytes = data.dtype.byteorder == "<" or (
+                    sys.byteorder == "little" and data.dtype.byteorder == "="
+                )
             # Copy is needed to prevent errors.
             if swapbytes:
                 return np.char.decode(data.copy().byteswap().view(dt), encoding)
@@ -1071,7 +1082,11 @@ def convert_to_numpy_bytes(  # noqa: C901, PLR0911, PLR0912
             for index, x in np.ndenumerate(data):
                 new_data[index] = np.bytes_(x.encode("UTF-8"))
             return new_data
-        if ndarray_has_type(data, np.uint8) or ndarray_has_type(data, np.uint16) or ndarray_has_type(data, np.uint32):
+        if (
+            ndarray_has_type(data, np.uint8)
+            or ndarray_has_type(data, np.uint16)
+            or ndarray_has_type(data, np.uint32)
+        ):
             # It is an ndarray of some uint type. How it is converted
             # depends on its shape. If its shape is just (), then it is
             # just a scalar wrapped in an array, which can be converted
@@ -1125,7 +1140,7 @@ def convert_to_numpy_bytes(  # noqa: C901, PLR0911, PLR0912
             # then decode to str first, if they are uint32, put them as an
             # input buffer for an ndarray of type 'U').
             data = data.ravel()
-            for i in range(new_data.shape[0]):  # type: ignore [misc]
+            for i in range(new_data.shape[0]):
                 chunk = data[(i * length2) : ((i + 1) * length2)]
                 new_data[i] = np.bytes_(convert_to_str(chunk).encode("UTF-8"))
 
